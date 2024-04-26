df7b12c81fac9684941c0bb64d4b88b0f0abeff0
seth-way <seth.r.way@gmail.com>
2024-04-26 12:38:37 -0400

    complete answers for ifStatements

--- ifStatements.js
+++ ifStatements.js
@@ -1,21 +1,23 @@
-// In the below exercises, write code that achieves the desired result. 
+// In the below exercises, write code that achieves the desired result.
 // To check your work, run this file by entering the following command in your Terminal: `node ifStatements.js`
 
 // CHALLENGE 1
 // Using the numQuarters variable defined below, determine if you have enough money to buy a gumball. A gumball costs two quarters.
 
-// Right now, the program will print out both "I have enough money for a gumball" and "I don't have enough money for a gumball". 
+// Right now, the program will print out both "I have enough money for a gumball" and "I don't have enough money for a gumball".
 // Write a conditional statement that prints only one or the other.
 // Experiment with manipulating the value held within numQuarters to make sure both conditions can be achieved.
 
 var numQuarters = 0;
 
-console.log("I have enough money for a gumball");
-console.log("I don't have enough money for a gumball");
-
+if (numQuarters >= 2) {
+  console.log('I have enough money for a gumball');
+} else {
+  console.log("I don't have enough money for a gumball");
+}
 
 // CHALLENGE 2
-// Using the variables defined below, write code that will tell you if you have the ingredients to make a pizza. 
+// Using the variables defined below, write code that will tell you if you have the ingredients to make a pizza.
 // A pizza requires at least two cups of flour and sauce.
 
 // You should be able to change the variables to achieve the following outputs:
@@ -29,3 +31,8 @@ console.log("I don't have enough money for a gumball");
 var cupsOfFlour = 1;
 var hasSauce = true;
 
+if (cupsOfFlour >= 2 && hasSauce) {
+  console.log('I can make pizza');
+} else {
+  console.log('I cannot make pizza');
+}
