9106c59dae6b06629bd79addcdcf33684db719e2
seth-way <seth.r.way@gmail.com>
2024-04-26 12:35:51 -0400

    complete answers for comparisons

--- comparisons.js
+++ comparisons.js
@@ -2,29 +2,42 @@
 // One example is already completed. Your task is to complete any remaining prompt.
 // Make sure to run the file in your command line using `node <filename>.js`
 
-
 // -------------------
 // PART 1: Comparing variables
 // -------------------
 
-var numberTeachers = 4
-var numberStudents = 20
-var stringTeachers = "4"
-var numberDogs = 0
+var numberTeachers = 4;
+var numberStudents = 20;
+var stringTeachers = '4';
+var numberDogs = 0;
 
 // EXAMPLE: log the result of the comparison: is numberTeachers greater than numberStudents?
-console.log("Is numberTeachers greater than numberStudents?", numberTeachers > numberStudents);
+console.log(
+  'Is numberTeachers greater than numberStudents?',
+  numberTeachers > numberStudents
+);
 // this should log: "Is numberTeachers greater than numberStudents?" false
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers?
+console.log(
+  'Is numberTeachers equal to stringTeachers?',
+  numberTeachers === stringTeachers
+);
 // this should log: false
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
+console.log(
+  'is numberTeachers not equal to numberStudents?',
+  numberTeachers !== numberStudents
+);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
+console.log(
+  'Is numberStudents greater than or equal to 21?',
+  numberStudents >= 21
+);
 // this should log: false
-
 
 // #-------------------
 // PART 2: Articulating what you are doing
@@ -34,14 +47,15 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 
 var friends = 6;
 var siblings = 2;
+// Determine if the number of friends is greater than the number of siblings.
 console.log(friends > siblings);
-// YOU DO: Explain.
+// this should log: true
 
 var attendees = 9;
 var meals = 8;
+// Determine if the number of attendees is not equal to the number of meals.
 console.log(attendees !== meals);
-// YOU DO: Explain.
-
+// this should log: true
 
 // #-------------------
 // PART 3: Logical Operators
@@ -63,10 +77,16 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
+console.log('Dog loves to play AND loves treats?', lovesToPlay && lovesTreats);
 
 // YOU DO:
 // Determine if the dog loves to play or loves the dog park
+console.log(
+  'Dog loves to play OR loves the dog park?',
+  lovesToPlay || lovesDogPark
+);
 
 // YOU DO:
 // Determine if the dog loves to play and is a puppy
 // HINT: Use the age variable and assume that a puppy is less than 2 years old
+console.log('Dog loves to play AND is a puppy?', lovesToPlay && age < 2);
